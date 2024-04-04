0c103682561d5f92dbdd49eea56cfe5aa0be0d35
Hassan <mhassanirshad1@gmail.com>
2024-04-04 19:50:20 +0500

    4th question in regex and 5th ques in sets

--- regex.py
+++ regex.py
@@ -72,3 +72,59 @@ You are not responsible for printing anything to stdout. Locked stub code in the
 
 regex_integer_in_range = r"^[1-9]\d{3,5}$"	# Do not delete 'r'.
 regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	# Do not delete 'r'.
+
+
+'''
+Neo has a complex matrix script. The matrix script is a N X M
+
+grid of strings. It consists of alphanumeric characters, spaces and symbols (!,@,#,$,%,&).
+
+[Capture.JPG]
+
+To decode the script, Neo needs to read each column and select only the alphanumeric characters and connect them. Neo reads the column from top to bottom and starts reading from the leftmost column.
+
+If there are symbols or spaces between two alphanumeric characters of the decoded script, then Neo replaces them with a single space '' for better readability.
+
+Neo feels that there is no need to use 'if' conditions for decoding.
+
+Alphanumeric characters consist of: [A-Z, a-z, and 0-9].
+
+Input Format
+
+The first line contains space-separated integers
+N(rows) and M(columns) respectively.
+The next N lines contain the row elements of the matrix script.
+
+Constraints
+0 < N, M < 100
+Note: A score will be awarded for using 'if' conditions in your code.
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+matrix = list(zip(*matrix))
+sample = str()
+for x in matrix:
+    for char in x:
+        sample += char
+print(re.sub(r'(?<=\w)([^\w\d]+)(?=\w)', ' ',sample))
+    
--- sets.py
+++ sets.py
@@ -42,3 +42,38 @@ for _ in range(n):
 print(sol)
 
 
+'''If we want to add a single element to an existing set, we can use the .add() operation.
+It adds the element to the set and returns 'None'.
+
+Example
+
+>>> s = set('HackerRank')
+>>> s.add('H')
+>>> print s
+set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+>>> print s.add('HackerRank')
+None
+>>> print s
+set(['a', 'c', 'e', 'HackerRank', 'H', 'k', 'n', 'r', 'R'])
+
+
+Task
+
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of
+
+country stamps.
+
+Find the total number of distinct country stamps.
+
+Input Format
+
+The first line contains an integer N, the total number of country stamps.
+The next lines contains the name of the country where the stamp is from. '''  
+
+N = int(input())
+a = set()
+for i in range(N):
+    a.add(input())
+print(len(a))
