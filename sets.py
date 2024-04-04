3069f2b7dd7b4f3cee93cee1626574c3ba22819f
Hassan <mhassanirshad1@gmail.com>
2024-04-04 20:59:36 +0500

    6th ques in sets

--- sets.py
+++ sets.py
@@ -77,3 +77,77 @@ a = set()
 for i in range(N):
     a.add(input())
 print(len(a))
+
+'''
+.remove(x)
+
+This operation removes element x from the set.
+If element x does not exist, it raises a KeyError.
+The .remove(x) operation returns None.
+
+Example
+
+>>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+>>> s.remove(5)
+>>> print s
+set([1, 2, 3, 4, 6, 7, 8, 9])
+>>> print s.remove(4)
+None
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+>>> s.remove(0)
+KeyError: 0
+
+.discard(x)
+
+This operation also removes element x from the set.
+If element x does not exist, it does not raise a KeyError.
+The .discard(x) operation returns None.
+
+Example
+
+>>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+>>> s.discard(5)
+>>> print s
+set([1, 2, 3, 4, 6, 7, 8, 9])
+>>> print s.discard(4)
+None
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+>>> s.discard(0)
+>>> print s
+set([1, 2, 3, 6, 7, 8, 9])
+
+.pop()
+
+This operation removes and return an arbitrary element from the set.
+If there are no elements to remove, it raises a KeyError.
+
+Example
+
+>>> s = set([1])
+>>> print s.pop()
+1
+>>> print s
+set([])
+>>> print s.pop()
+KeyError: pop from an empty set
+
+Task
+
+You have a non-empty set s, and you have to execute N commands given in N lines.
+
+The commands will be pop, remove and discard. 
+'''
+
+n = int(input())
+s = set(map(int, input().split()))
+for _ in range(int(input())):
+    entry = input().split()
+    if entry[0] == 'pop':
+        s.pop()
+    elif entry[0] == 'discard':
+        s.discard(int(entry[1]))
+    elif entry[0] == 'remove':
+        s.remove(int(entry[1]))
+print(sum(s))
